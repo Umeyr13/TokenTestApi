b4e862df035a326d1bf8413ee27672da9eba144a
Umeyr13 <umeyr.gulcimen@gmail.com>
2023-11-08 22:38:39 +0300

    RepositoryTemelleri

---
+++ DataAccessLayer/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IRepository<T>
+    {
+        Task<int> Insert(T nesne);
+        Task<int> Update(T nesne);
+        Task<int> Delete(T nesne);
+        Task<List<T>> Liste();
+        Task<List<T>> Liste(Expression<Func<T, bool>> kosul);
+        Task<T> Find(Expression<Func<T, bool>> kosul);
+      
+    }
+}
---
+++ DataAccessLayer/Repository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private DatabaseContext db;
+
+        private DbSet<T> _dbSet;
+        public Repository()
+        {
+          db = new DatabaseContext();
+            _dbSet = db.Set<T>();
+            
+        }
+
+        public async Task<int> Delete(T nesne)
+        {
+           
+            _dbSet.Remove( nesne );
+            return await db.SaveChangesAsync();
+        }
+
+        public async Task<T> Find(Expression<Func<T, bool>> kosul)
+        {
+           
+            return await _dbSet.FirstAsync(kosul);
+        }
+
+        public async  Task<int> Insert(T nesne)
+        {
+          
+            return await db.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> Liste()
+        {
+            
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<List<T>> Liste(Expression<Func<T, bool>> kosul)
+        {
+            return await _dbSet.Where(kosul).ToListAsync();
+        }
+
+
+
+        public async Task<int> Update(T nesne)
+        {
+            return await db.SaveChangesAsync();
+        }
+    }
+}
